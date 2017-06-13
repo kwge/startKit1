638e41b5187fec000ec721c55df8512cd5be832a
204070127 <kenny.wong@ge.com>
2017-06-13 11:55:23 -0700

    updated wct.conf

--- wct.conf.js
+++ wct.conf.js
@@ -1,5 +1,3 @@
-const DIR = __dirname.split("/").slice(-1)[0];
-
 module.exports = {
     verbose: true,
     plugins: {
@@ -26,52 +24,6 @@ module.exports = {
                 }
             ]
         }
-        //   istanbul: {
-        //     dir: "./coverage",
-        //     reporters: ["text-summary", "lcov"],
-        //     include: [
-        //       "/public/bm-ea-router.html",
-        //       "**/modules/ea-supervisor/views/polymer-components/**/*.html",
-        //       "**/modules/ea-supervisor/views/polymer-components/**/*.js"
-        //     ],
-        //     exclude: [
-        //       "/polymer/polymer.js",
-        //       "/platform/platform.js",
-        //       "/"
-        //     ],
-        //     thresholds: {
-        //       global: {
-        //         statements: 80,
-        //         //TODO: raise branches to 80
-        //         branches: 75,
-        //         functions: 80,
-        //         lines: 80
-        //       },
-        //       // First, meet these thresholds for each file
-        //       //each: {
-        //         //statements: 80,
-        //         //TODO: raise branches to 80
-        //         //branches: 75,
-        //         //functions: 80,
-        //         //lines: 80
-        //       //}
-        //     }
-        //   }
-        // },
-        // clientOptions: {
-        //   environmentScripts: [
-        //     'stacky/browser.js',
-        //     'async/lib/async.js',
-        //     'lodash/lodash.js',
-        //     'mocha/mocha.js',
-        //     'chai/chai.js',
-        //     'sinonjs/sinon.js',
-        //     'sinon-chai/lib/sinon-chai.js',
-        //     'accessibility-developer-tools/dist/js/axs_testing.js',
-        //     DIR.concat("/public/modules/ea-supervisor/utilities/bm_ea_utilities.js"),
-        //     DIR.concat("/test/fixtures/nav.js"),
-        //     DIR.concat("/test/fixtures/localeData.js"),
-        //     DIR.concat("/test/fixtures/bm_ea_utilities.js")
-        //   ]
+
     }
 };
