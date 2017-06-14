d173f24646f05d5d6cf2b6cfb724dcb78789464f
204070127 <kenny.wong@ge.com>
2017-06-14 10:57:20 -0700

    added android

--- wct.conf.js
+++ wct.conf.js
@@ -16,12 +16,15 @@ module.exports = {
                 "browserName": "internet explorer",
                 "platform": "Windows 8.1",
                 "version": "11"
-            },
-                {
-                    "browserName": "safari",
-                    "platform": "OS X 10.11",
-                    "version": "9"
-                }
+            }, {
+                "browserName": "safari",
+                "platform": "OS X 10.11",
+                "version": "9"
+            }, {
+                "browserName": "android",
+                "platform": "linux",
+                "version": "4.4"
+            }
             ]
         }
 
