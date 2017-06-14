9d76800a45eda82354cb6563467725342d242475
204070127 <kenny.wong@ge.com>
2017-06-14 15:16:31 -0700

    Added server.js -> Type "node server.js"

---
+++ server.js
@@ -0,0 +1,25 @@
+// Node.js notation for importing packages
+var express = require('express');
+
+// Spin up a server
+var app = express();
+
+
+// Serve static files from the main directory
+app.use(express.static(__dirname + '/build/default'));
+
+
+//  ____________________________________
+// |                                    |
+// |       Setting up Page Routes       |
+// |____________________________________|
+//
+
+app.get('*', function(req, res){
+    res.sendFile('index.html', {root: '.'});
+});
+
+// Tell the app to listen for requests on port 3000
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
--- wct.conf.js
+++ wct.conf.js
@@ -1,44 +1,44 @@
 module.exports = {
-    verbose: true,
-    plugins: {
-        local: {
-            browsers: ['chrome'],
-            seleniumPort: 4444,
-            skipSelenium: false // You have your own selenium server running
-        },
-        "sauce": {
-            "disabled": true,
-            "browsers": [{
-                "browserName": "microsoftedge",
-                "platform": "Windows 10",
-                "version": ""
-            }, {
-                "browserName": "internet explorer",
-                "platform": "Windows 8.1",
-                "version": "11"
-            }, {
-                "browserName": "safari",
-                "platform": "OS X 10.11",
-                "version": "9"
-            }, {
-                "browserName": "android",
-                "platform": "linux",
-                "version": "4.4"
-            }, {
-                "browserName": "firefox",
-                "platform": "Windows 7",
-                "version": "22"
-            }, {
-                "browserName": "iphone",
-                "platform": "OS X 10.9",
-                "version": "8.1"
-            }, {
-                "browserName": "ipad",
-                "platform": "OS X 10.9",
-                "version": "8.1"
-            }
-            ]
-        }
-
+  verbose: true,
+  plugins: {
+    local: {
+      browsers: ['chrome'],
+      seleniumPort: 4444,
+      skipSelenium: false // You have your own selenium server running
+    },
+    "sauce": {
+      "disabled": true,
+      "browsers": [{
+        "browserName": "microsoftedge",
+        "platform": "Windows 10",
+        "version": ""
+      }, {
+        "browserName": "internet explorer",
+        "platform": "Windows 8.1",
+        "version": "11"
+      }, {
+        "browserName": "safari",
+        "platform": "OS X 10.11",
+        "version": "9"
+      }, {
+        "browserName": "android",
+        "platform": "linux",
+        "version": "4.4"
+      }, {
+        "browserName": "firefox",
+        "platform": "Windows 7",
+        "version": "22"
+      }, {
+        "browserName": "iphone",
+        "platform": "OS X 10.9",
+        "version": "8.1"
+      }, {
+        "browserName": "ipad",
+        "platform": "OS X 10.9",
+        "version": "8.1"
+      }
+      ]
     }
+
+  }
 };
